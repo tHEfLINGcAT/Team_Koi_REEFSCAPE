d55a78873b8d7effc27252c00a91c3f6f6e6829b
Roi Romem <130078520+RoiRomem@users.noreply.github.com>
2025-02-19 10:15:35 +0200

    re added files into a new fork

--- Team_Koi_2025/src/main/java/frc/robot/Constants.java
+++ Team_Koi_2025/src/main/java/frc/robot/Constants.java
@@ -4,6 +4,10 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.util.Units;
+import swervelib.math.Matter;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -13,7 +17,30 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-  public static class OperatorConstants {
-    public static final int kDriverControllerPort = 0;
-  }
+    public static class OperatorConstants {
+        public static final int kDriverControllerPort = 0;
+        public static final double DEADBAND        = 0.1;
+        public static final double LEFT_Y_DEADBAND = 0.1;
+        public static final double RIGHT_X_DEADBAND = 0.1;
+        public static final double TURN_CONSTANT    = 6;
+    }
+    public static final class DrivebaseConstants
+    {
+
+        public static final double WHEEL_LOCK_TIME = 10; // seconds
+    }
+    public static final double ROBOT_MASS = (148 - 20.3) * 0.453592; // 32lbs * kg per pound
+    public static final Matter CHASSIS    = new Matter(new Translation3d(0, 0,Units.inchesToMeters(8)),ROBOT_MASS);
+    public static final double LOOP_TIME  = 0.13; //s, 20ms + 110ms sprk max velocity lag
+    public static final double MAX_SPEED  = Units.feetToMeters(14.5);
+
+    public static final class RobotHandConstants {
+        public static final int MOTOR_ID = 17;
+
+        public static final double GRAB_POWER = 1;
+        public static final double IDLE_POWER = 0.1;
+        public static final double RELEASE_POWER = 0.2;
+        public static final double STOPING_POWER = 0;
+    }
+
 }
